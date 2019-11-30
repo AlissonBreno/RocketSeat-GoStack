43a1d1e4d5a38be164ea06feb29f04fe494c098a
Alisson Breno <alissonbrenoemp@gmail.com>
2019-11-30 11:29:03 -0300

    Desafio finalizado

--- Dominando Node.js/desafio01/index.js
+++ Dominando Node.js/desafio01/index.js
@@ -4,26 +4,81 @@ const server = express();
 
 server.use(express.json());
 
-//const projects = ['Esses', 'São', 'Os Projetos'];
-const projects = [{
-  titlle: 'titulo 1',
-  tasks: 'fazer tal coisa'
-}];
+const projects = [];
+
+//Middleware que checa a existencia de um projeto
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+//Middleware global para contar requisições
+function logRequests(req, res, next) {
+
+  console.count("Número de requisições");
+
+  return next();
+}
+
+server.use(logRequests);
 
 //Lista todos os projetos do sistema
 server.get('/projects', (req, res) => {
   return res.json(projects);
 });
 
-//Altera um item da array
-server.put('/projects/:index', (req, res) => {
-  const { index } = req.params;
-  const { titlle } = req.body;
+//Cadastra novos projetos no Sistema
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
 
-  projects[index] = titlle;
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
 
-  return res.json(users);
+  projects.push(project);
+
+  return res.json(project);
 });
 
+//Cadastra tarefa em um projeto existente
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
 
-server.listen(3000);
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+//Altera um projeto pelo id 
+server.put('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+//Deleta um projeto pelo id
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  projects.splice(id, 1);
+
+  return res.send();
+});
+
+server.listen(4000);
